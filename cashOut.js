1137e98f8e2fd720328b8fc50499111ab05771e6
DilrubaShondha <dilrubaeasmin2867@gmail.com>
2024-09-23 19:10:59 +0600

    cashout commit

--- cashOut.js
+++ cashOut.js
@@ -1,18 +1,19 @@
 // step 1 - add event handler
-document.getElementById('button-add-money')
+document.getElementById('button-cash-out')
     .addEventListener('click', function(e){
         e.preventDefault();
         // step 2 - get money to the account
-        const amount = document.getElementById('amount').value;
+        const amount = document.getElementById('cashout-amount').value;
         const amountNumber = parseInt(amount);
-        const pin = document.getElementById('pin').value;
+
+        const pin = document.getElementById('pin-cashout').value;
         //temporary but have to validate pin
-        if (pin === '124'){
+        if (pin === '123'){
         //step 4 - get the account balance
         const balance = document.getElementById('balance').innerText;
         const balanceNumber = parseInt(balance);
         //step 5 - add money with balance
-        const newBalance = balanceNumber + amountNumber;
+        const newBalance = balanceNumber - amountNumber;
         // step 6 - update the new balance
         document.getElementById('balance').innerText = newBalance;
 
@@ -21,4 +22,4 @@ document.getElementById('button-add-money')
             alert('failed to transfer');
         }
        
-})
+});
