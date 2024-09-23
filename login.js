a8203fe08a9d115f981ba362371127c70976bc7f
DilrubaShondha <dilrubaeasmin2867@gmail.com>
2024-09-23 12:53:46 +0600

    update commit

--- login.js
+++ login.js
@@ -1,11 +1,22 @@
+// step 1 - set event hendler
 document.getElementById('button-login')
     .addEventListener('click', function(e){
+        // step 2 - prevent event hendler
         e.preventDefault();
         console.log('clicked');
+        // step 3 - get phone number & pin number
         const phoneNumber = document.getElementById('phone-number').value;
         console.log(phoneNumber);
 
         const pinNumber = document.getElementById('pin-number').value;
         console.log(pinNumber);
+        //temporary
+        if (phoneNumber === '5' && pinNumber === '124'){
+            console.log('You are logged in');
+            window.location.href = './home.html'
+        }
+        else{
+            alert('your credentials doesnot match');
+        }
 
 })
